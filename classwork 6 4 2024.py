0ddf5a62238e722cef53b536783dd60e1d347acd
gikako1 <nikavanishvilinika@gmail.com>
2024-06-04 20:13:46 +0400

    classwork

---
+++ classwork 6 4 2024.py
@@ -0,0 +1,12 @@
+#სამუშაო 1
+
+print(input("please enter password: "))
+print(input("please re-enter password: "))
+
+#სამუშაო 2
+
+print(input()>input())
+print(input()<input())
+print(input()>=input())
+print(input()<=input())
+print(input()==input())
---
+++ classwork.py
@@ -0,0 +1,10 @@
+#დავალება 1
+num1=input()
+num2=input()
+print(int(num1)+int(num2))
+
+#დავალება 2
+first_name=print(input("please enter your first name: "))
+last_name=print(input("please enter your last name: "))
+age=print(input("please enter your age: "))
+
